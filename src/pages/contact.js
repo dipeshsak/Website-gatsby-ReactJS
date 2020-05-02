1e9a84d22f92d0d8dd278228d8e2504db560fd6b
Dipesh <dipesh.sakharkar@otiselevator.com>
2020-05-02 15:44:44 +0530

    dummy contact

---
+++ src/pages/contact.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from '../components/Reuseable/HeroSection'
+import InfoBlock from '../components/Reuseable/InfoBlock'
+import DualInfoBlock from '../components/Reuseable/DualInfoBlock'
+import TeamPhotoSection from '../components/Reuseable/About/TeamPhotoSection'
+const Contact = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+    img={data.img.childImageSharp.fluid}
+    title="About Company"
+    subtitle=""
+    heroclass="about-backgrund"
+    />
+     <DualInfoBlock heading="A Message from CEO" connect="Connect Me"/>
+    <InfoBlock heading="About Vision"/>
+    <TeamPhotoSection/>
+    
+  </Layout>
+)
+export const query=graphql`
+{
+  img: file(relativePath: { eq: "about.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+  }
+    
+}
+`
+export default Contact
